a75fde6a24f9187ee601fd89f8152b9d41129353
天行者 <2249875980@qq.com>
2025-12-02 22:33:38 +0800

    add script

--- verify.js
+++ verify.js
@@ -14,7 +14,7 @@
           ⚠️ 此站点不是官方地址，请访问(This site is not an official address, please visit)<br>
           <a href="${OFFICIAL_SITE}" style="color:#007acc;">${OFFICIAL_SITE}</a>
         </div>`
-            setTimeout(() => (location.href = OFFICIAL_SITE), 3000)
+            setTimeout(() => (location.href = OFFICIAL_SITE), 1000)
         }
     } catch (err) {
         console.error('Domain verification failed:', err)
